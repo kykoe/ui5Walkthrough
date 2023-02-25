91f9359afd2dd471ce4d662c939694f109198033
Ky Koe <you@example.com>
2023-02-25 02:34:10 +0100

    Step 5: Add Controller

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,9 @@
-sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
-	"use strict";
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
+  "use strict";
 
-	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App"
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
+  XMLView.create({
+    viewName: "sap.ui.demo.walkthrough.view.App",
+  }).then(function (oView) {
+    oView.placeAt("content");
+  });
 });
